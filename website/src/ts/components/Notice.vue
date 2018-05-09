b6aa5850a7e25e615634ed5e9ff1eaf124d02635
Ugochukwu Remigius Chigbata <princeugo.remix@gmail.com>
2018-05-09 11:47:24 +0100

    Create Notice.vue

---
+++ website/src/ts/components/Notice.vue
@@ -0,0 +1,49 @@
+<template>
+	<div class="container article">
+		<div class="row">
+			<div style="width: 100%; position: relative;">
+				<h3>Notices</h3>
+				
+				<h4>Privacy Policy</h4>
+				
+				<p>SteemMakers is aimed at curating and supporting makers' and DIY’s quality contents in the Steem blockchain. We have prepared this privacy policy to help you understand the information we collect, and as well how we use this information when you use our service.</p>
+
+				<h5>General Audience Service</h5>
+				<p>Our service is open and intended to users of all age, gender and race.</p>
+
+				<h5>Security</h5>
+
+				<p>In the course of using our service, you will be redirected to <a href="https://steemconnect.com">SteemConnect</a> to authenticate to the blockchain. This authentication is Steemit Inc. and Busy.org project available on <a href="https://github.com/steemit/steemconnect">github</a>.
+				All access to our service is encrypted. This is done to assure our users that the exchanged information is secured.</p>
+
+				<h5>Cookies</h5>
+				<p>The SteemMakers' website uses cookies. These are small amounts of data that is stored on the user’s computer. These cookies are used for your convenience and not to track you or your behavior.</p>
+
+				<h4>Disclaimer</h4>
+				<p>SteemMakers does not store any log or user data whatsoever. We will never at any time, through our Discord channel or our community ask for your Steem private keys. Our Service do not contain links to other sites. SteemMakers is not in any way affiliated to any third party website.</p>
+
+				<h5>The contents on our website</h5>
+				<p>Some contents on our website is fetched from the Steem blockchain and published on our website for visibility and better discovery. This content may contain links to third party websites. The content provider, the creator of the blockchain transaction, is solely liable for the content. We are not liable for the precision or intention of the content whatsoever (including links or malicious content if any). Note that these websites are not operated by us. However, we reserve the right to terminate any publication if found inappropriate.</p>
+
+				<h4>Changes to these notices</h4>
+				<p>We may change these notices from time to time. If we do, we will provide a notice through our blog or Discord with immediate effect. However, we encourage you to review the notices whenever you use our website and interact with us to stay informed.</p>
+
+				<h5>Contact us</h5>
+				<p>If you have any questions or suggestions about our privacy policy, please do not hesitate to <a href="#/contact">contact us</a>.</p>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script lang="ts">
+	import Vue from "vue";
+	
+	export default Vue.extend({
+
+	});
+</script>
+
+
+<style scoped>
+
+</style>
